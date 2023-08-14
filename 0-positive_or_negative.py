f9c6f9ae10b94469d431fa04d41e901b639fcb07
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-14 08:46:41 -0500

    Positive and negative number

---
+++ 0-positive_or_negative.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+
+int main(void){
+	int number;
+
+	srand(time(0));
+	number = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative\n", number);
+	} 
+    else if (n == 0)
+	{
+		printf("%d is zero\n", number);
+	} else
+	{
+		printf("%d is positive\n", number);
+	}
+	return (0);
+}
