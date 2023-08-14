db19e43a066a0e07d61a07a9dc1eb78b0a86aaac
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-14 10:12:35 -0500

    update file.py

--- 0-positive_or_negative.py
+++ 0-positive_or_negative.py
@@ -1,24 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
 
-int main(void){
-	int number;
-
-	srand(time(0));
-    number = rand() - RAND_MAX / 2;
-
-	if (number > 0)
-	{
-		printf("%d is positive\n", number);
-	} 
-    else if (number == 0)
-	{
-		printf("%d is zero\n", number);
-	} else
-	{
-		printf("%d is negative\n", number);
-	}
-	return (0);
-}
+if (number > 0):
+	print("{} is positive\n".format(number))
+elif (number < 0):
+	print("{} is negative\n".format(number))
+else:
+	print("{} is zero\n".format(number))
